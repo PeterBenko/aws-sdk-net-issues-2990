e09620df17772b3d41b0c2ada9b31fa93b821422
Peter Benko <peter.benko@xitaso.com>
2023-07-04 18:17:08 +0200

    Repro solution aws-sdk-net/issues/2990

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Amazon.S3;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Exception when accessing Blob Storage.
+    /// </summary>
+    public class BlobStorageAccessException : Exception
+    {
+        /// <inheritdoc />
+        public BlobStorageAccessException(string message, AmazonS3Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
